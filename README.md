2cf1d88f38ec7e174544e4a93a74472cc725f3a2
swangek <swangek2002@gmail.com>
2026-02-10 16:20:17 -0500

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-## ADK wrapped agents
+Run commands in three terminals:
+
+Terminal 1: $env:OLLAMA_API_BASE="http://localhost:12345"; python main.py 
+
+Terminal 2: node frontend/copilot-server.mjs 
+
+Terminal 3: cd frontend && npm run dev
+
+Additionally, you need to maintain the SSH tunnel:
+
+Bash
+ssh -L 12345:yukon.acm.unc.edu:11434 YourOnyen@raptor.acm.unc.edu
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from backend!")
+# def main():
+#     print("Hello from backend!")
 
+
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+from ag_ui_adk import ADKAgent, add_adk_fastapi_endpoint
+from brain_network_chart.agent import root_agent
+import uvicorn
+
+adk_agent = ADKAgent(
+    adk_agent=root_agent,
+    app_name="brain_network_chart",
+    user_id="demo_user",
+    session_timeout_seconds=3600,
+    use_in_memory_services=True
+)
+
+app = FastAPI()
+add_adk_fastapi_endpoint(app, adk_agent, path="/")
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="localhost", port=8022)
